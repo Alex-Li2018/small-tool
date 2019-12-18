1f1a8a588f56ad7cd58d7b61db76658a12489f3d
alex-li2019 <alexlismile@163.com>
2019-12-18 16:43:30 +0800

    实现代码统计工具

--- src/statisticsCodeLine.js
+++ src/statisticsCodeLine.js
@@ -1,13 +1,16 @@
 /**
  * 统计代码行数的工具, 每个文件的,总数的
  */
+const { once } = require('events');
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 // 读取的目录文件
-const filePath = path.resolve('../../../Yao/src');
+const filePath = path.resolve('../../Yao/src');
 // 结果的文件
 const writeFilePath = './record.txt'
+// 总的行数
+let countLine = 0, fileCount = 0;
 
 // 读取文件
 readFileAndCountLine(filePath);
@@ -28,7 +31,8 @@ function readFileAndCountLine(filePath) {
                 }
                 if (stats.isFile()) {
                     // 如果是文件, 直接读取内容行数
-
+                    let fileLine = 0;
+                    readLine(fileDir, fileLine);
                 }
                 if (stats.isDirectory()) {
                     // 如果是目录,递归
@@ -37,6 +41,24 @@ function readFileAndCountLine(filePath) {
             });
         });
     });
+}
+
+// 读取每一行代码
+async function readLine(url, fileLine) {
+    // 创建interface类
+    const rl = readline.createInterface({
+        input: fs.createReadStream(url)
+    });
+
+    rl.on('line', (line) => {
+        line && countLine++;
+        line && fileLine++;        
+    });
+
+    rl.on('close', () => {
+        fileCount++;
+        console.log('总共扫描多少文件', fileCount, '当前文件的' + url + '行数',fileLine, '代码总行数', countLine);
+    })
 }
 
 // 记录文件
